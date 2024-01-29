f2cd6ca01f5825886f61e69d3ac8349a16fcd973
katyakan <katyakan86@gmail.com>
2024-01-29 11:21:52 +0600

    added compress.js file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,28 +1,48 @@
 import fs from 'fs';
 import zlib from 'zlib';
-import pipeline from 'stream';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
 
+const pipelineAsync = promisify(pipeline);
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pathToSource = __dirname + '/files/fileToCompress.txt';
+const pathToDestination = __dirname + '/files/compressed.gz';
 const compress = async () => {
-  const inputFilePath = './files/fileToCompress.txt';
-  const outputFilePath = './files/archive.gz';
+  try {
+    await fs.promises.access(pathToSource);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error(
+        `FS operation failed: source file ${pathToSource} does not exist`
+      );
+    }
+    throw err;
+  }
 
-  const readStream = fs.createReadStream(inputFilePath);
-
-  const writeStream = fs.createWriteStream(outputFilePath);
+  try {
+    await fs.promises.access(pathToDestination);
+    throw new Error(
+      `FS operation failed: target file ${pathToDestination} already exists`
+    );
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+  }
 
   const gzip = zlib.createGzip();
+  const source = fs.createReadStream(pathToSource);
+  const destination = fs.createWriteStream(pathToDestination);
 
-  await new Promise((resolve, reject) => {
-    pipeline(readStream, gzip, writeStream, (err) => {
-      if (err) {
-        console.error('Compression failed:', err);
-        reject(err);
-      } else {
-        console.log('File compressed successfully.');
-        resolve();
-      }
-    });
-  });
+  await pipelineAsync(source, gzip, destination);
+  console.log(
+    `FS operation success: file ${pathToSource} compressed to ${pathToDestination}`
+  );
 };
 
-await compress();
+await compress(pathToSource, pathToDestination);
